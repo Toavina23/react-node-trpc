2cd30883eaa2f27746bf2e776e0665d04df3e8a8
Toavina23 <zotoavina.andria@gmail.com>
2023-11-12 12:42:45 +0300

    changed server import path on client

--- client/src/trcp/trpc-client.ts
+++ client/src/trcp/trpc-client.ts
@@ -1,3 +1,3 @@
-import { type AppRouter } from "../../../server/src";
+import { type AppRouter } from "server/src";
 import { createTRPCReact } from "@trpc/react-query";
 export const trpc = createTRPCReact<AppRouter>();
